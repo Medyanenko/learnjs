545961724327967f182ac49d4ee77752ccc2979a
unknown <user@ptua-ls0280.kie.pt.playtech.corp>
2020-05-21 14:11:18 +0300

    add new file array.js

---
+++ array.js
@@ -0,0 +1,57 @@
+/*Давайте произведём 5 операций с массивом.
+Создайте массив styles с элементами «Джаз» и «Блюз».
+Добавьте «Рок-н-ролл» в конец.
+Замените значение в середине на «Классика». Ваш код для поиска значения в середине должен работать для массивов с любой длиной.
+Удалите первый элемент массива и покажите его.
+Вставьте «Рэп» и «Регги» в начало массива. */
+
+
+let styles = ['Джаз', 'Блюз'];
+styles.push('Рок-н-ролл');
+console.log(styles);
+styles[Math.floor((styles.length - 1) / 2)] = "Классика";
+console.log(styles);
+styles.shift();
+console.log(styles);
+styles.unshift("Рэп", "Регги");
+console.log(styles);
+
+/*Напишите функцию sumInput(), которая:
+Просит пользователя ввести значения, используя prompt и сохраняет их в массив.
+Заканчивает запрашивать значения, когда пользователь введёт не числовое значение, пустую строку или нажмёт «Отмена».
+Подсчитывает и возвращает сумму элементов массива.
+P.S. Ноль 0 – считается числом, не останавливайте ввод значений при вводе «0».*/
+
+
+function sumInput() {
+
+  let numbers = [];
+
+  while (true) {
+
+    let value = prompt("Введите число", 0);
+
+    if (value === "" || value === null || !isFinite(value)) break;
+
+    numbers.push(+value);
+  }
+
+  let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+alert(sumInput());
+
+/*Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString». */
+
+function camelize(str) {
+  return str
+    .split('-')
+    .map(
+      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join('');
+}
