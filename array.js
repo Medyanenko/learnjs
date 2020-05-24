468238fec10609856e05d958492020ef14a2c6bc
unknown <user@ptua-ls0280.kie.pt.playtech.corp>
2020-05-24 18:28:01 +0300

    add new tasks in  array.js

--- array.js
+++ array.js
@@ -55,3 +55,75 @@ function camelize(str) {
     )
     .join('');
 }
+
+
+/*Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
+Функция должна возвращать новый массив и не изменять исходный.*/
+
+function filterRange(arr, a, b) {
+  return arr.filter(item => (a <= item && item <= b));
+}
+
+/*Напишите функцию filterRangeInPlace(arr, a, b), которая принимает массив arr и удаляет из него все значения кроме тех, которые находятся между a и b. То есть, проверка имеет вид a ≤ arr[i] ≤ b.
+Функция должна изменять принимаемый массив и ничего не возвращать. */
+
+function filterRangeInPlace(arr, a, b) {
+
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+
+}
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4);
+console.log(arr);
+
+/*Сортировать в порядке по убыванию*/
+
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort((a, b) => b - a);
+
+alert(arr);
+
+/*У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
+Создайте функцию copySorted(arr), которая будет возвращать такую копию.*/
+
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert(sorted);
+alert(arr);
+
+/*У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.*/
+
+let vasya = {
+  name: "Вася",
+  age: 25
+};
+let petya = {
+  name: "Петя",
+  age: 30
+};
+let masha = {
+  name: "Маша",
+  age: 28
+};
+
+let users = [vasya, petya, masha];
+
+let names = users.map(item => item.name);
+
+alert(names);
